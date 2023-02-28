8eae69ac30191d8900a246e1127318f49657bcb6
Saeid Mohadjer <saeid@fastmail.fm>
2023-02-28 17:05:35 +0100

    bug fix

--- api/form-multipart-parser.js
+++ api/form-multipart-parser.js
@@ -1,17 +1,50 @@
 import multiparty from 'multiparty';
 import util from 'util';
+import fs from 'fs';
+import { Octokit, App } from "octokit";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const octokit = new Octokit({
+  auth: process.env.Token
+});
 
 export default (req, res) => {
     if (req.method === "POST") {
         let form = new multiparty.Form();
         form.parse(req, (err, fields, files) => {
             res.writeHead(200, { 'content-type': 'text/html' });
-            res.write('received upload: \n\n');
-            const path = files.upload[0].path
-            //console.log(files);
-            console.log(files.upload[0].path);
+
+            const file = files.upload[0];
+            console.log(file);
+
+            if (file.size > 10000000) {
+                res.end('file is too big');
+                return;
+            }
+
+            const source = fs.readFileSync(file.path);
+            const sourceBase64 = source.toString('base64');
+
+            octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+                owner: 'smohadjer',
+                repo: 's3',
+                path: file.originalFilename,
+                message: 'a new commit message',
+                committer: {
+                  name: 'Monalisa Octocat',
+                  email: 'octocat@github.com'
+                },
+                content: sourceBase64,
+                headers: {
+                  'X-GitHub-Api-Version': '2022-11-28'
+                }
+            });
+
 
             //res.write('<img src="' + path + '" />');
+            res.write('received upload: \n\n');
             res.end(util.inspect({ fields: fields, files: files }));
         });
         return;
