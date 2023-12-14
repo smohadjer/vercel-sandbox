604784e38b19adbea3b31b722747d0f8d880ff05
Saeid Mohadjer <saeid@fastmail.fm>
2023-12-14 15:56:38 +0100

    minor

--- api/staticdata.js
+++ api/staticdata.js
@@ -1,15 +1,14 @@
 import path from 'path';
 import fs from 'fs';
 
-export function getFileContent(filePath) {
-  const absolutePath = path.join(process.cwd(), 'public', filePath);
-  console.log(absolutePath);
+export function getFileContent(filename) {
+  const absolutePath = path.join(process.cwd(), 'public', 'json', filename);
   const fileContent =  fs.readFileSync(absolutePath, 'utf8');
   return fileContent;
 };
 
 export default async (req, res) => {
-  const fileContents = getFileContent('json/data.json');
+  const fileContents = getFileContent('public/json/data.json');
   //Return the content of the data file in json format
   res.status(200).json(fileContents);
 }
