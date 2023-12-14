16168ba44db6636c9b5502e3d4b0686690330741
Saeid Mohadjer <saeid@fastmail.fm>
2023-12-14 10:02:11 +0100

    bug fix

--- api/staticdata.js
+++ api/staticdata.js
@@ -2,7 +2,8 @@ import path from 'path';
 import fs from 'fs';
 
 export function getFileContent(filePath) {
-  const absolutePath = path.join(process.cwd(), filePath);
+  const absolutePath = path.join(process.cwd(), 'public', filePath);
+  console.log(absolutePath);
   const fileContent =  fs.readFileSync(absolutePath, 'utf8');
   return fileContent;
 };
